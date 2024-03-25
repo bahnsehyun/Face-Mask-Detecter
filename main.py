add886a88c8dcc00ef3ceed05dd0c507fead4d46
bahnsehyun <63949848+bahnsehyun@users.noreply.github.com>
2024-03-25 11:45:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # Touched by bahnse
-# test 1 - > test 2
+# test 1
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
